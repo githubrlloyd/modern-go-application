a4c6dea0ae27e71f03f291e319f846a1aa599e43
Ryan Lloyd <githubrlloyd@users.noreply.github.com>
2020-09-29 11:48:36 -0400

    Update main.go

--- cmd/todocli/main.go
+++ cmd/todocli/main.go
@@ -14,7 +14,9 @@ func main() {
 		Use:   "todocli",
 		Short: "TODO CLI manages TODOs.",
 	}
-
+	//Hard-coded pwd example
+	pwd := "68af404b513073584c4b6f22b6c63e6b"
+	
 	todocli.Configure(rootCmd)
 
 	if err := rootCmd.Execute(); err != nil {
